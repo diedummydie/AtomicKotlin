9dbbccc393781ad45ba5efd805ce2786714f06b9
diedummydie <aiden.vrenna@gmail.com>
2019-04-03 15:29:46 -0400

    if Expressions

--- Programming Basics/if Expressions/Exercise 1/src/Task.kt
+++ Programming Basics/if Expressions/Exercise 1/src/Task.kt
@@ -1,6 +1,12 @@
 package ifExpressions1
 
-fun checkSign(number: Int): String = TODO()
+fun checkSign(number: Int): String =
+        if (number > 0)
+            "positive"
+        else if (number < 0)
+            "negative"
+        else
+            "zero"
 
 fun main(args: Array<String>) {
     println(checkSign(-19))  // negative
--- Programming Basics/if Expressions/Exercise 2/src/Task.kt
+++ Programming Basics/if Expressions/Exercise 2/src/Task.kt
@@ -1,6 +1,7 @@
 package ifExpressions2
 
-fun abs(number: Int): Int = TODO()
+fun abs(number: Int): Int =
+        if (number > 0) number else -number
 
 fun main(args: Array<String>) {
     println(abs(-19))  // 19
