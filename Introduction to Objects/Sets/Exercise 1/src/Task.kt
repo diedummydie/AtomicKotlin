8cb40592f52776fa78018db6393ec8ef34b67384
diedummydie <aiden.vrenna@gmail.com>
2019-04-09 10:51:14 -0400

    sets

--- Introduction to Objects/Sets/Exercise 1/src/Task.kt
+++ Introduction to Objects/Sets/Exercise 1/src/Task.kt
@@ -6,11 +6,16 @@ val meats = setOf("beef", "chicken")
 val fruits = setOf("apple", "orange", "banana", "kiwi")
 val vegetables = setOf("beans", "peas", "carrots", "sweet potatoes", "asparagus", "spinach")
 
-fun percentMeat(groceryCart: Set<String>): Double = TODO()
-fun percentFruit(groceryCart: Set<String>): Double = TODO()
-fun percentVeggies(groceryCart: Set<String>): Double = TODO()
-fun percentOther(groceryCart: Set<String>): Double = TODO()
-
+private fun getPercentage(itemSet: Set<String>, groceryCart: Set<String>) =
+        100.0 * groceryCart.intersect(itemSet).size / groceryCart.size
+fun percentMeat(groceryCart: Set<String>): Double =
+        getPercentage(meats, groceryCart)
+fun percentFruit(groceryCart: Set<String>): Double =
+        getPercentage(fruits, groceryCart)
+fun percentVeggies(groceryCart: Set<String>): Double =
+        getPercentage(vegetables, groceryCart)
+fun percentOther(groceryCart: Set<String>): Double =
+        getPercentage(groceryCart - meats - fruits - vegetables, groceryCart)
 fun main(args: Array<String>) {
     val groceryCart = setOf("apple", "pretzels", "bread", "orange", "beef",
             "beans", "asparagus", "sweet potatoes", "spinach", "carrots")
--- Introduction to Objects/Sets/Exercise 2/src/Task.kt
+++ Introduction to Objects/Sets/Exercise 2/src/Task.kt
@@ -3,7 +3,7 @@ package sets2
 import atomictest.eq
 
 fun hasUniqueCharacters(s: String): Boolean {
-    TODO()
+    return s.length == s.toSet().size
 }
 
 fun main(args: Array<String>) {
