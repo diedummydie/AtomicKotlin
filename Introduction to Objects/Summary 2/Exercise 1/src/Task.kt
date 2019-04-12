511f0e01196d66aa8d96900ed134efe611a230c4
diedummydie <aiden.vrenna@gmail.com>
2019-04-11 22:21:25 -0400

    summary 2

--- Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 1/src/Task.kt
@@ -2,10 +2,41 @@ package summary21
 
 import atomictest.eq
 
-val intToRomanNumeralMap: Map<Int, String> = TODO()
+val intToRomanNumeralMap: Map<Int, String> = mapOf(
+        1000 to "M",
+        900 to "CM",
+        500 to "D",
+        400 to "CD",
+        100 to "C",
+        90 to "XC",
+        50 to "L",
+        40 to "XL",
+        10 to "X",
+        9 to "IX",
+        5 to "V",
+        4 to "IV",
+        1 to "I"
+)
+
+val intToRomanMinimal = mapOf(
+        1000 to "M",
+        500 to "D",
+        100 to "C",
+        50 to "L",
+        10 to "X",
+        5 to "V",
+        1 to "I"
+)
 
 fun convertToRoman(number: Int): String {
-    TODO()
+    var result = ""
+    var remain = number
+    for ((int, rom) in intToRomanNumeralMap) {
+        result += rom.repeat(remain / int)
+        remain %= int
+    }
+    println("$number: $result")
+    return result
 }
 
 fun main(args: Array<String>) {
@@ -13,4 +44,6 @@ fun main(args: Array<String>) {
     convertToRoman(44) eq "XLIV"
     convertToRoman(100) eq "C"
     convertToRoman(2018) eq "MMXVIII"
+    convertToRoman(45)
+    convertToRoman(79)
 }
--- Introduction to Objects/Summary 2/Exercise 2/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 2/src/Task.kt
@@ -2,10 +2,51 @@ package summary22
 
 import atomictest.eq
 
-val romanNumeralToIntMap: Map<String, Int> = TODO()
+val romanNumeralToIntMap: Map<String, Int> = mapOf(
+    "M" to 1000,
+    "CM" to 900,
+    "D"  to 500,
+    "CD" to 400,
+    "C"  to 100,
+    "XC" to 90,
+    "L"  to 50,
+    "XL" to 40,
+    "X"  to 10,
+    "IX" to 9,
+    "V"  to 5,
+    "IV" to 4,
+    "I"  to 1
+)
 
 fun convertFromRoman(roman: String): Int {
-    TODO()
+    var result = 0
+    for (i in 0 until roman.length) {
+        // Subtract if next Roman Numeral character is larger
+        val curr = romanNumeralToIntMap.getValue(roman[i].toString())
+        if (i+1 < roman.length) {
+            val next = romanNumeralToIntMap.getValue(roman[i+1].toString())
+            if (next > curr) {
+                result -= curr
+                continue
+            }
+        }
+        // Add since next is smaller or at end
+        result += curr
+    }
+    return result
+}
+
+fun betterFromRoman(roman: String): Int {
+    var result = 0
+    var max = 0
+    for (s in roman.reversed()) {
+        val int = romanNumeralToIntMap.getValue("$s")
+        if (int >= max) {
+            result += int
+            max = int
+        } else { result -= int }
+    }
+    return result
 }
 
 fun main(args: Array<String>) {
